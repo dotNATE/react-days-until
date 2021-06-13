8f24c5897e5a4a777b9f4c98583789a781b58366
Nathaniel Payne <somersetnathaniel@gmail.com>
2021-06-13 12:43:29 +0100

    added styling to Button, Header1, DateButtonsBox and index.css

--- src/Components/Atoms/Header1/index.tsx
+++ src/Components/Atoms/Header1/index.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import './Header1.css'
 
 type Header1Props  = {
     content: string
--- src/Components/Molecules/DateButtonsBox/index.tsx
+++ src/Components/Molecules/DateButtonsBox/index.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import './DateButtonsBox.css'
 
 type DateButtonsBoxProps = {
     children: any
