afdf84b6f7eb26d2fb797a53c559ddfa2f04206c
Nathaniel Payne <somersetnathaniel@gmail.com>
2021-06-20 15:24:08 +0100

    added SidebarItem into Sidebar Component

--- src/Components/Molecules/Sidebar/index.tsx
+++ src/Components/Molecules/Sidebar/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './Sidebar.css'
 import List from "../../Atoms/List";
+import SidebarItem from "./SidebarItem";
 
 type dateObject = {
     name: string,
@@ -10,12 +11,19 @@ type dateObject = {
 }
 
 type SidebarProps = {
-    dates: Array<dateObject>
+    dates: Array<dateObject>,
+    clicker: any
 }
 
-const Sidebar = ({ dates }: SidebarProps): JSX.Element => (
+const Sidebar = ({ dates, clicker }: SidebarProps): JSX.Element => (
     <List>
-
+        {
+            Array.isArray(dates) ? (
+                dates.map((date) => (
+                    <SidebarItem label={date.label} date={date.date} clicker={clicker} dates={date.dates} />
+                ))
+            ) : null
+        }
     </List>
 )
 
