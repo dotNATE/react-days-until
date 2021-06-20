126132674d43f1293f571d6b5761a34b0470113b
Nathaniel Payne <somersetnathaniel@gmail.com>
2021-06-20 15:19:50 +0100

    added SidebarItem Component

---
+++ src/Components/Molecules/Sidebar/SidebarItem/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import './SidebarItem.css'
+import ListItem from "../../../Atoms/ListItem";
+import ListItemText from "../../../Atoms/ListItemText";
+import List from "../../../Atoms/List";
+
+type dateObject = {
+    name: string,
+    label: string,
+    date: Date,
+    dates?: Array<dateObject>
+}
+
+type SidebarItemProps = {
+    label: string,
+    date: Date,
+    clicker: (date: Date, label: string) => void,
+    dates?: Array<dateObject>
+}
+
+const SidebarItem = ({label, clicker, date, dates: subDates}: SidebarItemProps): JSX.Element => (
+    <>
+    <ListItem>
+        <ListItemText label={ label } clicker={ () => clicker(date, label) } date={ date } />
+    </ListItem>
+        {
+            Array.isArray(subDates) ? (
+                <List>
+                    {
+                        subDates.map((subDate) => (
+                            <SidebarItem label={subDate.label} date={subDate.date} clicker={clicker} dates={subDate.dates}/>
+                        ))
+                    }
+                </List>
+            ) : null
+        }
+    </>
+)
+
+export default SidebarItem
